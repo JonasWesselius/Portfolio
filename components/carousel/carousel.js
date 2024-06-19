1bf9eb5857559e399a8e2ed25b9df0b2c680b314
JonasWesselius <112404721+JonasWesselius@users.noreply.github.com>
2024-06-19 05:33:50 +0200

    code cleanup

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -15,15 +15,13 @@ function showSlide(index, carouselId) {
         carousel.style.transform = `translateX(${offset}%)`;
     }
 }
-
-// Function to navigate to the previous slide in a carousel
 function prevSlide(carouselId) {
     const carousel = document.querySelector(`#${carouselId} .carousel-inner`);
     if (carousel) {
         const slides = carousel.querySelectorAll('.carousel-item');
         if (slides.length > 0) {
             let currentSlideIndex = Array.from(slides).findIndex(slide => slide.classList.contains('active'));
-            if (currentSlideIndex === -1) currentSlideIndex = 0; // Fallback to the first slide if none are active
+            if (currentSlideIndex === -1) currentSlideIndex = 0; 
             slides[currentSlideIndex].classList.remove('active');
             let prevSlideIndex = (currentSlideIndex === 0) ? slides.length - 1 : currentSlideIndex - 1;
             slides[prevSlideIndex].classList.add('active');
@@ -32,14 +30,13 @@ function prevSlide(carouselId) {
     }
 }
 
-// Function to navigate to the next slide in a carousel
 function nextSlide(carouselId) {
     const carousel = document.querySelector(`#${carouselId} .carousel-inner`);
     if (carousel) {
         const slides = carousel.querySelectorAll('.carousel-item');
         if (slides.length > 0) {
             let currentSlideIndex = Array.from(slides).findIndex(slide => slide.classList.contains('active'));
-            if (currentSlideIndex === -1) currentSlideIndex = 0; // Fallback to the first slide if none are active
+            if (currentSlideIndex === -1) currentSlideIndex = 0; 
             slides[currentSlideIndex].classList.remove('active');
             let nextSlideIndex = (currentSlideIndex === slides.length - 1) ? 0 : currentSlideIndex + 1;
             slides[nextSlideIndex].classList.add('active');
@@ -48,12 +45,10 @@ function nextSlide(carouselId) {
     }
 }
 
-// Initial slide
 document.addEventListener('DOMContentLoaded', function() {
-    showSlide(currentSlide, 'carousel1'); // Adjust 'carousel1' to the ID of your initial carousel
+    showSlide(currentSlide, 'carousel1'); 
 });
 
-// Event listeners for image click to open in lightbox
 document.querySelectorAll('.carousel-item img').forEach(img => {
     img.addEventListener('click', () => {
         const fullSrc = img.getAttribute('data-full');
@@ -64,7 +59,6 @@ document.querySelectorAll('.carousel-item img').forEach(img => {
     });
 });
 
-// Close lightbox on click outside the image
 document.getElementById('lightbox').addEventListener('click', () => {
     document.getElementById('lightbox').style.display = 'none';
 });
